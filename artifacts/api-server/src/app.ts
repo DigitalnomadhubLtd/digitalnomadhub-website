8f8f61f1298b1e76dcdfc59326dd0db3c1ab07a6
DigitalnomadhubLtd <Digitalnomadhub8@gmail.com>
2026-08-12 13:06:19 +0100

    Refactor pino-http import and serializers

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,8 +1,12 @@
 import express, { type Express } from "express";
+
 import cors from "cors";
-import pinoHttp from "pino-http";
+import * as pinoHttpModule from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
+
+const pinoHttp =
+  (pinoHttpModule as any).default ?? (pinoHttpModule as any);
 
 const app: Express = express();
 
@@ -10,14 +14,14 @@ app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req) {
+      req(req: any) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: any) {
         return {
           statusCode: res.statusCode,
         };
@@ -25,6 +29,7 @@ app.use(
     },
   }),
 );
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
